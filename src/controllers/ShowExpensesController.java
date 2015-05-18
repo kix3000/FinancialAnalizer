1a9a0619b9ebb9f12551b97cfeed8e6f253f897f
Michal Chojnacki <kix3000@gmail.com>
2015-05-18 20:46:59 +0200

    Table function in fxml.

--- src/controllers/ShowExpensesController.java
+++ src/controllers/ShowExpensesController.java
@@ -14,33 +14,18 @@ import java.util.ResourceBundle;
 
 public class ShowExpensesController implements Initializable{
 
-    @FXML private TableColumn colDate;
-    @FXML private TableColumn colAmount;
-    @FXML private TableColumn colPlace;
-    @FXML private TableColumn colDescription;
-    @FXML private ObservableList<Expense> tablelist;
+    @FXML private ObservableList<Expense> expensesTableData;
 
     public ShowExpensesController() {
-
-
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String[] variableNames = { "date", "amount", "place", "description" };
 
-        int i=0;
-
-        colDate.setCellValueFactory(new PropertyValueFactory<Expense, Double>(variableNames[i++]));
-        colAmount.setCellValueFactory(new PropertyValueFactory<Expense, String>(variableNames[i++]));
-        colPlace.setCellValueFactory(new PropertyValueFactory<Expense, String>(variableNames[i++]));
-        colDescription.setCellValueFactory(new PropertyValueFactory<Expense, BargainDate>(variableNames[i++]));
-
-        tablelist.add(new Expense(6.66,"Biedronka","lol"));
     }
 
     public void setData(ObservableList<Expense> ed){
-        tablelist.addAll(ed);
+        expensesTableData.addAll(ed);
     }
 }
